71b1d995b78030b7d7f379cc87d213c95f020c72
Basil Rari <73436584+basilrari@users.noreply.github.com>
2023-12-09 23:24:17 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -8,11 +8,11 @@ contract DisputeToken is ERC20, Ownable {
     constructor() ERC20("DisputeToken", "DPT") Ownable(msg.sender) {
     _mint(msg.sender, 1000000 * 10 ** decimals());
 }
-
+    //giving token to user who raise dispute or approve the work
     function raiseDispute(address user) public  onlyOwner {
         _mint(user, 1 * 10 ** decimals());
     }
-
+    //burning token after voting
     function resolveDispute(address user) public  onlyOwner {
         _burn(user, balanceOf(user));
     }
