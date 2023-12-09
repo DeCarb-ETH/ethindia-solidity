b3ae111a00c98e43284d0bcf92f0d724a466bbe9
Basil Rari <basilrari@gmail.com>
2023-12-09 07:08:18 +0530

    Initialising

---
+++ main.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract safeTransfer {
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function deposit() public payable {
+        require(msg.value == 0.5 ether, "Deposit amount must be 0.4 ETH");
+    }
+
+    function withdraw() public {
+        require(msg.sender == owner, "Only the owner can withdraw");
+        require(address(this).balance >= 1 ether, "Not enough balance");
+        owner.transfer(0.5 ether);
+    }
+}
