0e94dc3edfd9c0c8c664130598588dad5d7b0e70
Basil Rari <basilrari@gmail.com>
2023-12-09 07:48:02 +0530

    Added Provision to add Scientist Details

--- main.sol
+++ main.sol
@@ -1,15 +1,48 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract safeTransfer {
-    address payable public owner;
-
+contract ResearchPaperRegistry {
+    struct Scientist {
+        string name;
+        string[] researchPaperCIDs;
+    }
     constructor() {
         owner = payable(msg.sender);
     }
+    address payable public owner;
+    mapping(address => Scientist) public scientists;
+
+    // Function to add a new scientist
+    function registerScientist(string memory scientistName) external {
+        require(bytes(scientistName).length > 0, "Scientist name must not be empty");
+
+        Scientist storage scientist = scientists[msg.sender];
+        scientist.name = scientistName;
+    }
+
+    // Function to add a new research paper CID for an existing scientist
+    function addResearchPaper(string memory newResearchPaperCID) external {
+        Scientist storage scientist = scientists[msg.sender];
+        require(bytes(scientist.name).length > 0, "Scientist is not registered");
+
+        scientist.researchPaperCIDs.push(newResearchPaperCID);
+    }
+
+    // Function to get the number of research papers for a scientist
+    function getResearchPaperCount(address scientistAddress) external view returns (uint256) {
+        return scientists[scientistAddress].researchPaperCIDs.length;
+    }
+
+    // Function to get a specific research paper CID for a scientist
+    function getResearchPaperCID(address scientistAddress, uint256 index) external view returns (string memory) {
+        Scientist storage scientist = scientists[scientistAddress];
+        require(index < scientist.researchPaperCIDs.length, "Invalid index");
+
+        return scientist.researchPaperCIDs[index];
+    }
 
     function deposit() public payable {
-        require(msg.value == 0.5 ether, "Deposit amount must be 0.5 ETH");
+        require(msg.value == 0.5 ether, "Deposit amount must be 0.4 ETH");
     }
 
     function withdraw() public {
@@ -17,4 +50,5 @@ contract safeTransfer {
         require(address(this).balance >= 1 ether, "Not enough balance");
         owner.transfer(0.5 ether);
     }
+
 }
